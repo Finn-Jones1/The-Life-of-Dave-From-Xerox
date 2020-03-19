6d66458b6725c1903bdaaf540e0cc66250be8a8e
finn-jones <58809658+finn-jones@users.noreply.github.com>
2020-03-19 13:28:24 +1100

    First Faze almost done

--- AdventureGame.py
+++ AdventureGame.py
@@ -4,6 +4,7 @@ from pygame import mixer
 
 # Variables
 health = 100
+careerP = 1
 
  
 # Music/Sound Effects
@@ -12,7 +13,7 @@ mixer.init()
 alarm = pygame.mixer.Sound("/Users/finn.jones/Documents/AdventureGame/The-Life-of-Dave-From-Xerox/Alarm.wav")
 music = pygame.mixer.music.load("/Users/finn.jones/Documents/AdventureGame/The-Life-of-Dave-From-Xerox/ThemeSong.wav")
 
-pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.set_volume(0.3)
 pygame.mixer.music.play(-1)
 
 
@@ -29,6 +30,10 @@ def loading():
 def healthUpdate(damage):
     global health
     health = health + damage
+
+def CareerUpdate(career):
+    global careerP
+    careerP = careerP + career
 
 
 
@@ -54,16 +59,17 @@ def H2P():
     GameText(u"\u001b[32mTo check these stats just type stats\u001b[0m")
     GameText(u"\u001b[32mThis game is about a guy who is down on his luck and is trying to get through life\u001b[0m")
     GameText(u"\u001b[32mThe aim of the game is to make your way from Janitor to Ceo at Xerox by acumulating career points without letting your HP get to 0\u001b[0m")
-    
+    GameText(u"\u001b[32mPress Enter To Start\u001b[0m")
+    input("")
+    phaze1()
     
 
-def intro():
-    # mixer.music.load('/Users/finn.jones/Documents/Adventure Game/Alarm.mp3')
+def phaze1():
     alarm.play()
     GameText(u"\u001b[32mYou wake up to the sound you have grown to hate\u001b[0m")
     GameText(u"\u001b[32mThe alarm clock\u001b[0m")
     
-    GameText(u"\u001b[32mDo you (Punch or Snooze) the alarm clock:\u001b[0m")
+    GameText(u"\u001b[32mDo you (Punch) the alarm clock or do you (Wake) up:\u001b[0m")
     ans = input()
     ans = ans.lower()
 
@@ -71,15 +77,33 @@ def intro():
         GameText(u"\u001b[32mYou get electricuted\u001b[0m")
         healthUpdate(-50)
         GameText(u"\u001b[32mYour health is now "+  str(health)+ " HP\u001b[0m")
-    elif ans == "snooze":
-        print("")
+    elif ans == "wake":
+        rand = random.randint(8, 6)
+        GameText(u"\u001b[32mYou wake up and check the time"+ str(rand) +"\u001b[0m")
+        if rand > 7:
+            GameText(u"\u001b[32mYou are running late should you eat (Breakfast) or (Leave) without\u001b[0m")
+            ans = input("")
+            ans = ans.lower()
+            if ans == "breakfast":
+                GameText(u"\u001b[32mYou eat breakfast and gain 20 health but you are now running 20 minutes late\u001b[0m")
+                healthUpdate(20)
+            elif ans == "leave":
+                GameText(u"\u001b[32mYou skip breakfast and go straight to work you just make it on time.\u001b[0m")
+        else:
+            if ans == "breakfast":
+                GameText(u"\u001b[32mYou eat breakfast and gain 20 health and you get to work on time\u001b[0m")
+                GameText(u"\u001b[32mYour health is now "+  str(health)+ " HP\u001b[0m")
+            elif ans == "leave":
+                GameText(u"\u001b[32mYou skip breakfast and go straight to work you are early by 10 minutes this makes a good impression on your boss and you he says\u001b[0m")
+                GameText(u"\u001b[32mI like to see a deticated worker I am going to make you head of janitor management\u001b[0m")
+                CareerUpdate(1)
+                GameText(u"\u001b[32mYou gain a career point you now have "+  str(careerP) + " points\u001b[0m")
 
 
 
 
-def Level1():
-    GameText("asd")
 
 
-intro()
+
+H2P()
 
