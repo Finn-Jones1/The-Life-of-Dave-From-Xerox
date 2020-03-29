b98bf28534a6bc354c62eaf6b14adc97656190e7
BuildTools <unconfigured@null.spigotmc.org>
2020-03-29 11:58:15 +1100

    code has more structure and has more branches

--- AdventureGame.py
+++ AdventureGame.py
@@ -4,6 +4,7 @@ from pygame import mixer
 
 
 # Variables
+phaseCount = 0
 health = 100
 careerP = 1
 FlexyPath = os.getcwd()
@@ -17,7 +18,12 @@ pygame.mixer.music.play(-1)
 
 
 #Functions
-
+def GameOver():
+    for letter in (u"\u001b[31mGame Over\u001b[0m"):
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print("")
 
 def loading():
     print ("Loading...")
@@ -33,6 +39,9 @@ def healthUpdate(damage):
 def CareerUpdate(career):
     global careerP
     careerP = careerP + career
+    if careerP == 0:
+        GameOver()
+        exit()
 
 
 
@@ -51,25 +60,40 @@ def GameText(text):
         time.sleep(rand)
     print("")
 
+
+
 def shop():
+    global buy
     buy = ''
     loopS = 1
     while loopS == 1:
-        if buy != "1" or buy != "2" or buy != "3" or buy != "4" or buy != "exit":
-            loopS = 2
         GameText(u"\u001b[35mShop\u001b[0m")
         GameText(u"\u001b[33m1. Rewind 50HP\u001b[0m")
         GameText(u"\u001b[24m2. Wrench 50HP\u001b[0m")
         GameText(u"\u001b[36;1m3. Skip Fase 1 99HP\u001b[0;0m")
-        GameText(u"\u001b[1m1, 2, 3 or exit:\u001b[0m")
-        buy = input("")
+        GameText(u"\u001b[35m1, 2, 3 or exit:")
+        buy = input("\u001b[0m")
 
-    # if buy == "1":
-        
-    # elif buy == "2"
-    # elif buy == "3"
-    # elif buy == "4"
-    # elif buy == "exit"
+        if buy == "1":
+            GameText(u"\u001b[35mYou Now Have Rewind\u001b[0m")
+            loopS = 2
+        elif buy == "2":
+            GameText(u"\u001b[35mYou Now Have Wrench\u001b[0m")
+            loopS = 2
+        elif buy == "3":
+            GameText(u"\u001b[35mSkip Fase 1\u001b[0m")
+            loopS = 2
+        elif buy == "4":
+            print("not yet implemented")
+            loopS = 2
+        elif buy == "exit":
+            loopS = 2
+
+# def powerups():
+#     if buy == "1":
+#         asdf
+
+
 def H2P():
     GameText(u"\u001b[32mThis is\u001b[0m")
     GameText(u"\u001b[32mLife of Dave from Xerox\u001b[0m")
@@ -79,14 +103,15 @@ def H2P():
     GameText(u"\u001b[32mThe aim of the game is to make your way from Janitor to Ceo at Xerox by acumulating career points without letting your HP get to 0\u001b[0m")
     GameText(u"\u001b[32mPress Enter To Start\u001b[0m")
     input("")
-    phaze1()
+    phase1()
     
 
 # def phaze1():
     # alarm.play()
     # GameText(u"\u001b[32mYou wake up to the sound you have grown to hate\u001b[0m")
     # GameText(u"\u001b[32mThe alarm clock\u001b[0m")
-def phaze1():
+def phase1():
+    phaseCount = 1
     GameText(u"\u001b[32mDo you (Punch) the alarm clock or do you (Wake) up:\u001b[0m")
     ans = input()
     ans = ans.lower()
@@ -105,8 +130,8 @@ def phaze1():
         print("")
         YouDied()
     elif ans == "wake":
-        rand = random.randrange(6, 8)
-        GameText(u"\u001b[32mYou wake up and check the time "+ str(rand) +"\u001b[0m")
+        rand = random.randrange(5, 9)
+        GameText(u"\u001b[32mYou wake up and check the time your watch reads "+ str(rand) +":00\u001b[0m")
         if rand > 7:
             GameText(u"\u001b[32mYou are running late should you eat (Breakfast) or (Leave) without\u001b[0m")
             ans = input("")
@@ -114,6 +139,9 @@ def phaze1():
             if ans == "breakfast":
                 GameText(u"\u001b[32mYou eat breakfast and gain 20 health but you are now running 20 minutes late\u001b[0m")
                 healthUpdate(20)
+                GameText(u"\u001b[32mWhen you get to work you look at the time you are 40 minutes late\u001b[0m")
+                GameText(u"\u001b[32mYour boss waliks up to you and says 'I have never seen a more idotic indifferent worker in my life you are FIRED'\u001b[0m")
+                CareerUpdate(-1)
             elif ans == "leave":
                 GameText(u"\u001b[32mYou skip breakfast and go straight to work you just make it on time.\u001b[0m")
         else:
@@ -125,16 +153,19 @@ def phaze1():
                 GameText(u"\u001b[32mYour health is now "+  str(health)+ " HP\u001b[0m")
             elif ans == "leave":
                 GameText(u"\u001b[32mYou skip breakfast and go straight to work you are early by 10 minutes this makes a good impression on your boss and you he says\u001b[0m")
-                GameText(u"\u001b[32mI like to see a deticated worker I am going to make you head of janitor management\u001b[0m")
+                GameText(u"\u001b[32mI like to see a deticated worker, have you ever done printer matnence before?\u001b[0m")
+                GameText(u"\u001b[32mYou respond with 'I am fairly handy and printers can't be too hard'\u001b[0m")
+                GameText(u"\u001b[32m'Then it's settled you are now part of the printer matnence crew.'\u001b[0m")
                 CareerUpdate(1)
                 GameText(u"\u001b[32mYou gain a career point you now have "+  str(careerP) + " points\u001b[0m")
 
 
-
+def phase2():
+    print("phase 2")
 # rand = random.randrange(6, 8,)
 # print(rand)
-shop()
-# phaze1()
+# shop()
+phase1()
 
 
 # H2P()
